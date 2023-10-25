02aaee4610bc6108ad19464d2ea7b86b645ba92d
Rapee Wannasiri <rapeew@hotmail.com>
2023-10-25 13:59:57 +0700

    optimizing contour approximation as a function

--- utils.py
+++ utils.py
@@ -53,3 +53,44 @@ def getCvSample(fileName: str, dstPath: str) -> str:
         str: Local path to file.
     """
     return getGithubFile("opencv", "opencv", "master", f"samples/data/{fileName}", dstPath)
+
+
+
+def optimApproxPoly(vertice: typing.List[np.ndarray], 
+                    maxAreaDiff: float, 
+                    startE: float, 
+                    stepE: float, 
+                    maxIter: int, 
+                    verbose: bool = False) -> typing.Tuple[np.ndarray, float, float, float]:
+    """Reduce vertices by approximating a contour until area reach maxinum area difference.
+
+    Args:
+        vertice (typing.List[np.ndarray]): An array of contour vertice.
+        maxAreaDiff (float): Maximum area difference between original contour and approximated contour.
+        startE (float): Starting coefficient to calculate approximation.
+        stepE (float): Increment step of the coefficient.
+        maxIter (int): Maximum number of iterations.
+        verbose (bool, optional): Print results. Defaults to False.
+
+    Returns:
+        typing.Tuple[np.ndarray, float, float, float]: (Approximated contour vertice, last coefficient, last epsilon, last area different ratio)
+    """
+    # init.
+    orgArcLen = cv2.arcLength(vertice, True)
+    orgArea = cv2.contourArea(vertice)
+    areaDiff = 0.0
+    e = startE
+    i = 0
+    # optimize.
+    while (areaDiff < maxAreaDiff and i < maxIter):
+        eps = e * orgArcLen
+        approx = cv2.approxPolyDP(vertice , eps, True)
+        area = cv2.contourArea(approx)
+        areaDiff = np.abs(area - orgArea) / orgArea
+        e += stepE
+        i += 1
+    if verbose: 
+        print(f"From area {orgArea} to {area}.")
+        print(f"From vertice count {len(vertice)} to {len(approx)}.")
+        print(f"e = {e}\neps = {eps}\narea diff = {areaDiff}")
+    return approx, e, eps, areaDiff
